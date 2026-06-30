92174eb43a2e563ecb8efa0b7883019e9ff66049
Amaury Huerb Simões <amaury.simoes@grupomultilaser.com.br>
2026-06-30 06:29:20 -0400

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      // Esta é a linha mágica que libera o popup do Google falar com o seu localhost
+      "Cross-Origin-Opener-Policy": "same-origin-allow-popups"
+    }
+  }
 })
