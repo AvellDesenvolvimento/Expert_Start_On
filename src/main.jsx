4076151da5d0ddb55e434e6430bf876496a8f04f
Amaury Huerb Simões <amaury.simoes@grupomultilaser.com.br>
2026-07-02 07:41:38 -0400

    Ajuste Rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import App from './app.jsx';
 import './expert.css';
 
 // Força o modo dark ao iniciar
 document.body.classList.add('dark-mode');
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
